2c718999e829f1224b301ff38e77d49c44be149c
paghe <alessandro.paghera99@gmail.com>
2024-09-05 14:41:28 +0200

    wip/adding style to the home page

--- projects/models.py
+++ projects/models.py
@@ -7,3 +7,4 @@ class Project(models.Model):
     description = models.TextField()
     technology = models.CharField(max_length=20)
     image = models.FileField(upload_to="project_images/", blank=True)
+    github_url = models.URLField()
